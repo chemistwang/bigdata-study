3087439bf02b9092a5510e6ca43c3368b0850f02
y.wang <y.wang@lvdong.io>
2021-02-09 14:33:21 +0800

    first scala

---
+++ src/main/java/Scala/TestScala.scala
@@ -0,0 +1,14 @@
+package Scala
+
+/**
+  * @ClassName: TestScala
+  * @Description: TODO
+  * @Author: wyl
+  * @Date: 2021-02-09 14:22
+  * @Version 0.1
+  **/
+object TestScala {
+  def main(args: Array[String]): Unit = {
+    println("hello, scala !")
+  }
+}
